c61e5aa656d783917796f524cbd601f3cc49f8c3
zloypelmen <pettykinsasa822@gmail.com>
2024-01-08 00:30:00 +0300

    feat: add middlewares, aut with jwt

---
+++ backend/routes/index.js
@@ -0,0 +1,9 @@
+const Router = require('express')
+const router = new Router()
+const userRouter = require('./userRouter')
+const eventRouter = require('./eventRouter')
+
+router.use('/user', userRouter)
+router.use('/event', eventRouter)
+
+module.exports = router
