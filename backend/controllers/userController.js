44b93dced7003a353f618b41b875f4bf3811f0a2
zloypelmen <pettykinsasa822@gmail.com>
2024-01-09 02:34:27 +0300

    fix: fix problem with jwt auth and registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -30,7 +30,7 @@ class UserController {
 
     async login(req, res, next) {
         const {email, password} = req.body
-        const candidate = await User.findOne({ where: { email: email } });
+        const user = await User.findOne({ where: { email: email } });
 
         if (!user) {
             return next(ApiError.internal('Пользователь не найден'))
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -5,12 +5,11 @@ module.exports = function (req, res, next) {
         next()
     }
     try {
-        const token = req.headers.authorization.split(' ')[1] // Bearer asfasnfkajsfnjk
+        const token = req.headers.authorization.split(' ')[1] // Bearer
         if (!token) {
             return res.status(401).json({message: "Не авторизован"})
         }
-        let decoded;
-        decoded = jwt.verify(token, process.env.SECRET_KEY);
+        const decoded = jwt.verify(token, process.env.SECRET_KEY)
         req.user = decoded
         next()
     } catch (e) {
