f72ee43ff861e44a92535973ebeec426b7f98bbf
zloypelmen <pettykinsasa822@gmail.com>
2024-01-15 13:35:38 +0300

    !don't use not working version

--- frontend/src/pages/Calendar.js
+++ frontend/src/pages/Calendar.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from "react";
+
+import { getMonth } from "../utils/util";
+import CalendarHeader from "../components/CalendarHeader";
+import Sidebar from "../components/SideBar";
+import Month from "../components/Month";
+import GlobalContext from "../context/GlobalContext";
+import EventModal from "../components/EventModal";
 
 const Calendar = () => {
+    const [currenMonth, setCurrentMonth] = useState(getMonth());
+    const { monthIndex, showEventModal } = useContext(GlobalContext);
+
+    useEffect(() => {
+        setCurrentMonth(getMonth(monthIndex));
+    }, [monthIndex]);
+
     return (
-        <div>
-            Calendar
-        </div>
+        <React.Fragment>
+            {showEventModal && <EventModal />}
+
+            <div className="h-screen flex flex-col">
+                <CalendarHeader />
+                <div className="flex flex-1">
+                    <Sidebar />
+                    <Month month={currenMonth} />
+                </div>
+            </div>
+        </React.Fragment>
     );
 };
 
